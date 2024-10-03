9ef540ce22911f7ff39603ad6dc11b72de8c90a5
laxmanpradhan784 <laxmanpradhan784@gmail.com>
2024-10-03 11:58:12 +0530

    new

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,10 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>
-        this first page in my laravel
-    </h1>
-    <a href="/welcome">welcome page</a>
-    <a href="/about/laxman">about page</a>
+    <h1>this about page</h1>
+    <h1>{{$name}}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+// Route::view('/index','index');
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+// for small line
+
+// Route::view('/about','about');
+
+// Route::get('/about/', function () {
+//     return view('about');
+// });
+
+// Route::get('/about/{name}', function ($name) {
+//     echo $name;
+//     return view('about');
+// });
+
+Route::get('/about/{name}', function ($name) {
+    return view('about', ['name' => $name]);
+});
+
+//this redirect method
+
+// Route::redirect('/welcome','/index');
